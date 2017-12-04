58e7419b690a803a1950e9e82f509afb41449fdd
mmsamiei <mohmahsamiei@gmail.com>
2017-12-04 13:47:40 +0330

    :/

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+        autoplay: true,
+        autoplayTimeout: 2000,
+        nav: true,
+        autoplayHoverPause: true,
+        items : 5,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3],
+        rtl: true
+    });
+});
