6acdeba419bc0656781a0a74b44377385344a05c
mmsamiei <mohmahsamiei@gmail.com>
2017-12-06 00:53:28 +0330

    update ReadMe for @1995parham :)

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # WP_F17_HW3
-This phase of project is dedicated to **ImamKhomeini**(Peace be upon him)
+This phase of project is dedicated to **ImamKhomeini**(Peace be upon him) and also to **her:heart:**
 
 ## useful links that helped me in project workflow
 how to avoid conflict between Bootstrap and our manual css! ( how to make bootstrap isolate)
 
 https://formden.com/blog/isolate-bootstrap
+
+Add/Remove Items to OwlCarousel
+
+https://github.com/OwlCarousel2/OwlCarousel2/issues/339
 
 ## Important Point!
 jquery must has been linked before OWL!!!
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-
 $(document).ready(function(){
-    let owl = $(".owl-carousel");
+    let owl = $("#owl-carousel-father-div");
     owl.owlCarousel({
 
         autoplay: true,
@@ -21,5 +20,66 @@ $(document).ready(function(){
         console.log("mamad 1");
         owl.trigger('next.owl.carousel');
     };
+
+    let films = [
+        {
+            title :"Transformers",
+            year : 2007
+        },
+        {
+            title: "Frozen",
+            year: 2013
+        },
+        {
+            title :"Tangled",
+            year : 2010
+        },
+        {
+            title:"Toy Story 3",
+            year: 2010
+        },
+        {
+            title:"Monsters University",
+            year: 2013
+        },
+        {
+            title:"La La Land",
+            year: 2016
+        },
+        {
+            title:"The Amazing Spider-Man",
+            year: 2012
+        },
+        {
+            title:"Captain America: Civil War",
+            year: 2016
+        }
+
+    ];
+
+    for (let i=0;i<films.length ;i++){
+        console.log(JSON.stringify(films[i]))
+        console.log(films[i].title)
+
+        // let xhttp = new XMLHttpRequest();
+        // xhttp.open("GET", "http://www.theimdbapi.org/api/find/movie?title="+films[i].title+"&year="+films[i].year, false);
+        // xhttp.send();
+        // let response = JSON.parse(xhttp.responseText);
+
+        let content = '<div class="item"> <img src='+'https://images-na.ssl-images-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg'+'alt="Owl Image"> </div>'
+        // let owl = $("#owl-carousel-father-div");
+        $('#owl-carousel-father-div')
+            .owlCarousel('add', content)
+            .owlCarousel('update')
+        // owl.append(content);
+
+        url = "http://www.theimdbapi.org/api/find/movie?title="+films[i].title+"&year="+films[i].year;
+        $.get(url).done(function (object){
+            // console.log(object[0]["poster"]["thumb"]);
+            let content = '<div class="item"> <img src='+'https://images-na.ssl-images-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg'+'alt="Owl Image"> </div>'
+
+        });
+
+    }
 });
 
